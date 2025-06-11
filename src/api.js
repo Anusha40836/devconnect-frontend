96197da37214d1854c38734c4ed146cf489e777b
Anusha40836 <anusha96866@gmail.com>
2025-06-11 14:16:47 +0530

    Added /api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://devconnect-backend-cy4x.onrender.com/",
+  baseURL: "https://devconnect-backend-cy4x.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
